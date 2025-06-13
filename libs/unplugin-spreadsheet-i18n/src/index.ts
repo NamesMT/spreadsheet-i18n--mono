eea7d1bc898e55be9435855e3f41e516a024807a
NamesMT <dangquoctrung123@gmail.com>
2025-06-13 04:48:18 +0000

    fix: `handleHotUpdate` processing every file

--- libs/unplugin-spreadsheet-i18n/src/index.ts
+++ libs/unplugin-spreadsheet-i18n/src/index.ts
@@ -3,9 +3,11 @@ import type { Options } from './types'
 import { resolve } from 'pathe'
 import { processSheetFile, scanConvert } from 'spreadsheet-i18n'
 import { createUnplugin } from 'unplugin'
+import { createFilter } from 'vite'
 
 export const unpluginFactory: UnpluginFactory<Options | undefined> = (options) => {
   let cwd = resolve()
+  const filter = createFilter(options?.include, options?.exclude)
 
   return {
     name: 'unplugin-spreadsheet-i18n',
@@ -18,6 +20,7 @@ export const unpluginFactory: UnpluginFactory<Options | undefined> = (options) =
         await processSheetFile({
           filePath: file,
           options,
+          filter,
           cwd,
         })
       },
